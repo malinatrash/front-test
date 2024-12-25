2f8b039941269e33d06aa091ad4c6a9eb19f8990
Pavel Naumov <malinatrashh@gmail.com>
2024-12-25 15:27:13 +0800

    добавлены все зависимости + структура папок по чистой архитектуре + path aliases

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-createApp(App).mount('#app')
+import App from '@/presentation/views/App.vue'
+
+const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+
+app.mount('#app')
---
+++ src/presentation/views/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+  </main>
+</template>
+
+<style>
+body {
+  margin: 0;
+  background: var(--surface-ground);
+  color: var(--text-color);
+  font-family: var(--font-family);
+}
+</style>
